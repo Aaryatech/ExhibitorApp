e35f8a4e4326138b8e577775881fd547716933e9
Aaryatech <code@atsinfosoft.com>
2018-05-29 18:18:18 +0530

    changes //atul

--- app/src/main/java/com/ats/exhibitorapp/activity/SplashActivity.java
+++ app/src/main/java/com/ats/exhibitorapp/activity/SplashActivity.java
@@ -6,29 +6,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ats.exhibitorapp.R;
+import com.ats.exhibitorapp.utils.AppPreferences;
 
 public class SplashActivity extends AppCompatActivity {
 
     private int SPLASH_TIME_OUT = 1500;
+    private AppPreferences appPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        appPreferences=new AppPreferences(getApplicationContext());
+
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
- //test comment.
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-
+                if(appPreferences.isLoggedIn()) {
+                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                }else{
+                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                }
             }
         }, SPLASH_TIME_OUT);
 
